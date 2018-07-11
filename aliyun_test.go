9db74b3dbae7d1df24cdf16651dcd5026da6da2d
yayanyang <org.lemon@gmail.com>
2018-07-11 23:43:50 +0800

    update

--- aliyun_test.go
+++ aliyun_test.go
@@ -1,6 +1,18 @@
 package aliyun
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/dynamicgo/go-config/source/file"
+
+	config "github.com/dynamicgo/go-config"
+)
+
+func init() {
+	config := config.NewConfig()
+
+	config.Load(file.NewSource(file.WithPath("../config/aliyun.json")))
+}
 
 func TestAliyun(t *testing.T) {
 
