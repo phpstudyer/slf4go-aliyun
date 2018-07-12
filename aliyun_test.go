1762b787c22b464b78bfd8eba0459c455960e046
yayanyang <yayanyang@gmail.com>
2018-07-12 10:08:58 +0800

    update

--- aliyun_test.go
+++ aliyun_test.go
@@ -3,17 +3,65 @@ package aliyun
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
+	"github.com/dynamicgo/slf4go"
+
 	"github.com/dynamicgo/go-config/source/file"
 
 	config "github.com/dynamicgo/go-config"
 )
 
-func init() {
-	config := config.NewConfig()
+var logconfig config.Config
 
-	config.Load(file.NewSource(file.WithPath("../config/aliyun.json")))
+func init() {
+	logconfig = config.NewConfig()
+
+	logconfig.Load(file.NewSource(file.WithPath("../config/aliyun.json")))
 }
 
+var message = `
+hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world
+  hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world
+  hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world
+  hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world
+  hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world
+hello hello worldhello worldhello worldhello worldhello worldhello 
+worldhello worldhello worldhello worldworldhello worldhello worldhello
+ worldhello worldhello worldhello worldhello worldhello worldhello worldhello
+  worldhello worldhello worldhello worldhello worldhello worldhello worldhello 
+  worldhello worldhello worldhello world`
+
 func TestAliyun(t *testing.T) {
+
+	err := slf4go.Load(logconfig)
+
+	require.NoError(t, err)
+
+	logger := slf4go.Get("test")
+
+	for {
+		logger.DebugF("test")
+	}
 
 }
